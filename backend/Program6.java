ccda6696bc16d648c1013d8749c6af457ec2db63
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-04 20:16:45 +0530

    day 11

--- backend/Program6.java
+++ backend/Program6.java
@@ -21,6 +21,7 @@ unary operator
            1.preincrement- first it will increase the value by 1 then it will use
            2.postincrement-first it will use then it increase the value by 1
    2.decrement unary operator
-           1.predecrement
-           2.postdecrement
+           1.predecrement-first it will decrease the value by 1 then it will use
+           2.postdecrement-first it will use then it decrease the value by 1
+
 */
