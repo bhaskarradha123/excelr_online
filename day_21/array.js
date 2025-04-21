21812a0aae5c1d860081e3cb4abd58848f86396d
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-21 19:18:59 +0530

    day 21

--- day_21/array.js
+++ day_21/array.js
@@ -57,11 +57,32 @@
 // console.log(arr9.at(-1));//50
 // console.log(arr9.at(8));//undefined
 
-let arr1 = [10, 20, 30, 40, 50];
-let squareArr = arr1.map((num, index,arr1) => num * num);
-console.log(arr1, squareArr); // [100, 400, 900, 1600, 2500]
+// let arr1 = [10, 20, 30, 40, 50];
+// let squareArr = arr1.map((num, index,arr1) => num * num);
+// console.log(arr1, squareArr); // [100, 400, 900, 1600, 2500]
 
-let arr2=['this','is','array','session'];
-let arr3=arr2.map((e) =>e.toUpperCase() );
-console.log(arr3);//[ 'THIS', 'IS', 'ARRAY', 'SESSION' ]
+// let arr2=['this','is','array','session'];
+// let arr3=arr2.map((e) =>e.toUpperCase() );
+// console.log(arr3);//[ 'THIS', 'IS', 'ARRAY', 'SESSION' ]
+
+
+let arr1 = [10, 21, 36, 45, 50];
+// let arr2=arr1.filter(
+//     (e,index,arr1)=>{
+//          return e%2==0;//even numbers
+//     }
+// )
+let arr2=arr1.filter(num=>num%2==0)
+console.log(arr1);
+console.log(arr2);
+
+
+let inputArr=[6,7,8,9,10];
+let cubeArr=inputArr.map(e=>e*e*e);
+let oddArr=inputArr.filter(e=>e%2!=0);
+
+let outputArr=inputArr.map(e=>e*e*e).filter(e=>e%2!=0);
+console.log(inputArr);
+console.log(outputArr);
+
 
