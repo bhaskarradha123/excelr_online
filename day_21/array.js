2b76e3419d52fd3a2993e4b35ead05174fd722a0
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-21 18:19:52 +0530

    day 21

--- day_21/array.js
+++ day_21/array.js
@@ -1,35 +1,51 @@
-let arr1=[10,20,30,40,50]
+// let arr1=[10,20,30,40,50]
 
-console.log(arr1);
+// console.log(arr1);
 
-let arr2=['hlo','world','this','is','array']
-console.log(arr2);
-console.log(arr2[0]);
-console.log(arr2[1]);
-console.log(arr1.length);//5
-//creating array using new keyword
-let arr3=new Array(10,20,30,40,50);
-console.log(arr3);
+// let arr2=['hlo','world','this','is','array']
+// console.log(arr2);
+// console.log(arr2[0]);
+// console.log(arr2[1]);
+// console.log(arr1.length);//5
+// //creating array using new keyword
+// let arr3=new Array(10,20,30,40,50);
+// console.log(arr3);
 
-let arr4=new Array(3);//[_,_,_]
-console.log(arr4);//[empty × 3]
-console.log(arr4.length);//3
+// let arr4=new Array(3);//[_,_,_]
+// console.log(arr4);//[empty × 3]
+// console.log(arr4.length);//3
 
 
-let arr5=new Array(1);//[_]
-arr5[0]=10;
-console.log(arr5);//[10]//array containing
+// let arr5=new Array(1);//[_]
+// arr5[0]=10;
+// console.log(arr5);//[10]//array containing
 
-let arr6=Array.of('developer');
-console.log(arr6);//developer
+// let arr6=Array.of('developer');
+// console.log(arr6);//developer
 
-//accessing array values/elements/items
-let arr7=[10,20,30,40,50];
-console.log(arr7[0]);// with the help of index
-arr7[1]=10000;//updating the value of index 1
-console.log(arr7[1]);//10000
-arr7[1]=20000;//updating the value of index 2
-console.log(arr7[1]);//20000
+// //accessing array values/elements/items
+// let arr7=[10,20,30,40,50];
+// console.log(arr7[0]);// with the help of index
+// arr7[1]=10000;//updating the value of index 1
+// console.log(arr7[1]);//10000
+// arr7[1]=20000;//updating the value of index 2
+// console.log(arr7[1]);//20000
+
+
+//adding values to array
+let arr8=['this','is','array','session' ];
+console.log(arr8);
+arr8[4]=1000;
+arr8[9]=200;
+console.log(arr8.length);//10
+console.log(arr8[7]);//undefined
+
+arr8.push('example');//adding value to the end of array
+
+console.log(arr8);//[ 'this', 'is', 'array', 'session', 1000, <4 empty items>, 'example' ]
+
+arr8.unshift('hello');//adding value to the beginning of array
+console.log(arr8);
 
 
 
