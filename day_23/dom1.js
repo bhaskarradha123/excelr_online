fd7833a45ef5967a0992f4554a844dd85df7b63b
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-23 18:29:50 +0530

    day23

--- day_23/dom1.js
+++ day_23/dom1.js
@@ -12,3 +12,20 @@ for(let e of heading2){
 }
 
 console.log(heading2);
+
+//accessing element with getElementsByTagName()
+let heading3=document.getElementsByTagName('h3');
+for(let e of heading3){
+    e.style.color='blue';
+}
+
+//accessing element with querySelector()
+let p=document.querySelector('.p1');
+p.style.color='red';
+//accessing element with queryAllSelector()
+let pList=document.querySelectorAll('.p1');
+for (let e of pList) {
+    e.style.fontSize='40px';
+}
+
+
