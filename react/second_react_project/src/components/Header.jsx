7b1849cd13499017f9abc2ebef72c7409afd163d
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-30 17:38:13 +0530

    uploading day 28

---
+++ react/second_react_project/src/components/Header.jsx
@@ -0,0 +1,20 @@
+//functional component
+function Header(){
+ 
+    let menus=["Home","About","Contact","Services","Portfolio"];
+    return (
+        <>
+            <h1>Header</h1>
+            <ul>
+                {
+                    menus.map(
+                        (element,index,array)=><li>{element}</li>
+                    )
+                }
+            </ul>
+
+        </>
+    )
+}
+
+export default Header;
