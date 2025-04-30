a429cdecfee7648655afa021425b978091124e2a
Excelr Solutions <mailmeasradha@gmail.com>
2025-04-30 17:56:26 +0530

    uploading notes

--- react/second_react_project/src/components/Header.jsx
+++ react/second_react_project/src/components/Header.jsx
@@ -1,20 +1,16 @@
 //functional component
-function Header(){
- 
-    let menus=["Home","About","Contact","Services","Portfolio"];
-    return (
-        <>
-            <h1>Header</h1>
-            <ul>
-                {
-                    menus.map(
-                        (element,index,array)=><li>{element}</li>
-                    )
-                }
-            </ul>
-
-        </>
-    )
-}
+const Header = () => {
+  let menus = ["Home", "About", "Contact", "Services", "Portfolio"];
+  return (
+    <>
+      <h1>Header</h1>
+      <ul>
+        {menus.map((element, index, array) => (
+          <li>{element}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Header;
