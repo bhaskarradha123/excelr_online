ef022725817750e5e5d9823852ea609dfe280622
Excelr Solutions <mailmeasradha@gmail.com>
2025-05-08 18:09:01 +0530

    day 33

--- react/single_page_application_2/src/components/FetchProducts.jsx
+++ react/single_page_application_2/src/components/FetchProducts.jsx
@@ -1,28 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const FetchProducts = () => {
-    let[products,setProducts]=React.useState([]);
-    let fetchData=()=>{
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(json=>setProducts(json))//adding products to state
-        .catch(err=>console.log(err));
-    }
+  let [products, setProducts] = React.useState([]);
+  //fetching backend data with the help of fetch api
+  let fetchData = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json)) //adding products to state
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(()=>{
-        fetchData();
-    },[])
   return (
     <>
-   {
-    products.map(
-        (element,index)=>{
-            return <div key={index}>{element.title}</div>
-        }
-    )
-   }
+      {products.map((element, index) => {
+        return <div key={index}>{element.title}</div>;
+      })}
     </>
-  )
-}
+  );
+};
 
-export default FetchProducts
+export default FetchProducts;
