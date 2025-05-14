fb30efda28997aecdc754f71e4c35a2de3a0775e
Excelr Solutions <mailmeasradha@gmail.com>
2025-05-14 18:57:20 +0530

    day 36

--- react/redux_1/src/App.jsx
+++ react/redux_1/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import{useDispatch, useSelector} from 'react-redux'
+import { decrement, increment } from './actions/CounterAction';
 function App() {
- 
-
+  const dispatch=useDispatch();
+   const count=useSelector((state)=>state.count);
   return (
     <>
-      <button className='btn btn-outline-success'>+</button>
-      <button className='btn btn-outline-danger'>-</button>
+     <div>{count}</div>
+      <button onClick={()=>dispatch(increment())} className='btn btn-outline-success'>+</button>
+      {/* <button onClick={()=>dispatch({type:'INCREMENT'})} className='btn btn-outline-success'>+</button> */}
+      {" "}
+      <button onClick={()=>dispatch(decrement())} className='btn btn-outline-danger'>-</button>
     </>
   )
 }
--- react/redux_1/src/reducers/CounterReducer.jsx
+++ react/redux_1/src/reducers/CounterReducer.jsx
@@ -7,12 +7,14 @@ const initialState = {
 // it is a pure function which takes the previous state
 //  and an action as arguments and returns the next state
 const CounterReducer = (state = initialState, action) => {
-    if(action.type=== increment.type){ 
+    if(action.type===increment().type){ 
      return {count : state.count + 1}
     }
-    else if(action.type === decrement.type){
+    else if(action.type ===decrement().type){
         return {count : state.count - 1}
     }
+
+    return state;
 }
 
 export default CounterReducer;
