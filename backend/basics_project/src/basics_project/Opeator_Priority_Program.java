c7e6ed39e30e82955495bbf0ad85fa6dac840462
Excelr Solutions <mailmeasradha@gmail.com>
2025-05-23 19:37:33 +0530

    java variables and operators

--- backend/basics_project/src/basics_project/Opeator_Priority_Program.java
+++ backend/basics_project/src/basics_project/Opeator_Priority_Program.java
@@ -5,6 +5,6 @@ public class Opeator_Priority_Program {
 	public static void main(String[] args) {
 		System.out.println(10+20);//30
 		System.out.println(10+20*3);//70
-		System.out.println((10+20)*3);
+		System.out.println((10+20)*3);//90
 	}
 }
