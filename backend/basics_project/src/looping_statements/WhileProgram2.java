96f1ef9db8f6d3da487c26c37596a962284d652d
Excelr Solutions <mailmeasradha@gmail.com>
2025-05-26 20:02:21 +0530

    statements

---
+++ backend/basics_project/src/looping_statements/WhileProgram2.java
@@ -0,0 +1,13 @@
+package looping_statements;
+
+public class WhileProgram2 {
+
+	public static void main(String[] args) {
+//		implicit casting byte--->char
+		char value=65;
+		 while(value<=90) {
+			 System.out.println(value);
+			 value++;
+		 }
+	}
+}
