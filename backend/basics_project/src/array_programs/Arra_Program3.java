6c38194692d317e8553da8b8c12de9c6c6383d88
Excelr Solutions <mailmeasradha@gmail.com>
2025-05-28 18:14:49 +0530

    arrays

--- backend/basics_project/src/array_programs/Arra_Program3.java
+++ backend/basics_project/src/array_programs/Arra_Program3.java
@@ -1,9 +1,13 @@
 package array_programs;
 
+import java.util.Arrays;
+
 public class Arra_Program3 {
 
 	public static void main(String[] args) {
 		String []colors= {"red","yellow","pink","orange","blue"};
+
+
 		for(int i=0;i<colors.length;i++) {
 			System.out.println(colors[i]);
 		}
