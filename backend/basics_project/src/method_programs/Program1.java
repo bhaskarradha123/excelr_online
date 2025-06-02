6bb22facbbcfe991b7ba5cf3a8f23460d5801cc0
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-02 17:41:02 +0530

    methods and oops

--- backend/basics_project/src/method_programs/Program1.java
+++ backend/basics_project/src/method_programs/Program1.java
@@ -2,5 +2,17 @@ package method_programs;
 
 public class Program1 {
 
+	public static void main(String[] args) {
+		System.out.println("main method");
+		m1();
+		m2(10);
+	}
+	
+	public static void m1() { 
+		System.out.println("m1 method");	
+	}
+	public static void m2(int a) {
+		System.out.println("m2 method"+a);
+	}
 	
 }
