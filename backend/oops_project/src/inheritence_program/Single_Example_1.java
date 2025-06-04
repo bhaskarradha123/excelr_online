dfd37a3d320ca3e1192ae3cdf22a633c2a22b7e6
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-04 18:50:42 +0530

    oops concept day3

--- backend/oops_project/src/inheritence_program/Single_Example_1.java
+++ backend/oops_project/src/inheritence_program/Single_Example_1.java
@@ -3,18 +3,33 @@ public class Single_Example_1 {
 	public static void main(String[] args) {
 		Parent_1 p=new Parent_1();
 		p.m1();// m1 is parent
+		p.m2();
 		Child_1 c=new Child_1();
 		c.m1();	//m1 is override in child class
+		c.m2();
 	}}
 
 class Parent_1{
 	public void m1() {
 		System.out.println("m1 in parent");
-	}}
+	}
+
+	public final void m2() {
+		System.out.println("m2 in parent");
+	}
+}
 
 class Child_1 extends Parent_1{
 	@Override
 	public void m1() {
 //		super.m1();  -- parent method implementation
 		System.out.println("m1 is override in child class");
-	}}
+	}
+	
+//    @Override  --final methods can't be override
+//    public void m2() {
+//    	// TODO Auto-generated method stub
+//    	super.m2();
+//    }
+
+}
---
+++ backend/oops_project/src/inheritence_program/Single_Example_2.java
@@ -0,0 +1,24 @@
+package inheritence_program;
+
+public class Single_Example_2  extends Object{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	public static void main(String[] args) {
+		Single_Example_2 s=new Single_Example_2();
+//		s.clone();
+//		s.equals(s);
+//		s.finalize();
+		
+		
+	}
+}
