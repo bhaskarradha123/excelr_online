c0d4f9e8761c5ee2a27300c38e34aad9f9f05d86
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-05 18:40:12 +0530

    OOPS concept day 4

--- backend/oops_project/src/abstarction_program/Main.java
+++ backend/oops_project/src/abstarction_program/Main.java
@@ -2,20 +2,19 @@ package abstarction_program;
 
 public class Main {
 	public static void main(String[] args) {
-
 //		Employee employee=new Employee("BOB","bob@gmail.com",345678665,786);
-		
 		Developer developer=new Developer("Bob", "bob@gmail.com", 23456789, 753, "java");
 		developer.work();
+		
+		Employee employee=new Developer("Allen", "allen@gmail.com", 34567, 56, "python");
+		employee.work();
 	}
 }
-
 abstract class Employee {
 	String name;
 	String email;
 	long phone;
 	int id;
-
 	public Employee(String name, String email, long phone, int id) {
 		this.email = email;
 		this.name = name;
@@ -25,22 +24,13 @@ abstract class Employee {
 	}
 	public abstract void work();
 }
-
-
-
-
-
 class Developer extends Employee {
 	String langugage;
-	
-	
 	public Developer(String name, String email, long phone, int id, String language) {
 		super(name, email, phone, id);
 		this.langugage = language;
 		System.out.println("const in developer class");
 	}
-	
-	
 	@Override
 	public void work() {
 		System.out.println(name+" developer working on "+langugage);	
