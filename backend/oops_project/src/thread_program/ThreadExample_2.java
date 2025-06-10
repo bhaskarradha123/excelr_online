e310ac134418083c4d28ebe97977060e82a7a247
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-10 20:05:06 +0530

    threads

--- backend/oops_project/src/thread_program/ThreadExample_2.java
+++ backend/oops_project/src/thread_program/ThreadExample_2.java
@@ -8,7 +8,10 @@ public class ThreadExample_2 {
 
 }
 class Thread2 extends Thread{
-	
+	@Override
+	public void run() {
+		System.out.println("no args method");
+	}
 	
 	public void run(int i) {
 		System.out.println("args method");
