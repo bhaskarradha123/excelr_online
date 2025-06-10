5e83e49851f78b68567b4de89fd1632d56b24e55
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-10 18:41:25 +0530

    threads

---
+++ backend/oops_project/src/thread_program/ThreadLifeCycleMain.java
@@ -0,0 +1,34 @@
+package thread_program;
+
+public class ThreadLifeCycleMain {
+
+	public static void main(String[] args) {
+		 ThreadLifeCycle cycle=new ThreadLifeCycle();
+	       cycle.setName("Thread 1");
+	       System.out.println(cycle.getName()+"- new state"); // new state
+	       
+	       cycle.start();    
+	}
+}
+// user defined thread
+class ThreadLifeCycle extends Thread {
+	
+	@Override
+	public void run() {
+		System.out.println(getName()+ "- running state");
+		try {
+		Thread.sleep(5000);// thread is in waiting phase
+		System.out.println(getName()+"- woke up from sleeping");
+		
+	
+//		wait();
+//		System.out.println("moved from waiting state");
+		}
+		catch (Exception e) {
+			
+		}
+		
+		System.out.println(getName()+" is terminated");
+	}
+
+}
