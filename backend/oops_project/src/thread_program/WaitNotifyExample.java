1dc809ba36a46c091a1b6b57d0fee8d2949c7fbc
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-11 19:21:10 +0530

    thread class 2

--- backend/oops_project/src/thread_program/WaitNotifyExample.java
+++ backend/oops_project/src/thread_program/WaitNotifyExample.java
@@ -3,6 +3,17 @@ package thread_program;
 public class WaitNotifyExample {
 	static Object lock = new Object();
 
+	/*
+	 * synchronized(lock): it ensures mutual execution, 
+	 *                     so only one thread can hold the lock on the object at a time
+	 *                     so , one thread won't disturb another thread
+	 * 
+	 * wait():it causes the current thread to release the lock , by making a thread to wait
+	 *        until another thread calls notify ()
+	 * 
+	 * notify():wake up a waiting thread  and moves to running state
+	 * 
+	 * */
 	public static void main(String[] args) {
 		Thread waiter = new Thread(() -> {
 			synchronized (lock) {
