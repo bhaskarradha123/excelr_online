d74a84e205967b2badb4673e2cb5fca0db686e83
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-17 19:59:09 +0530

    map

--- backend/basics_project/src/array_programs/FindLongestPalindrome.java
+++ backend/basics_project/src/array_programs/FindLongestPalindrome.java
@@ -11,6 +11,19 @@ public class FindLongestPalindrome {
 		     }
 		}
 		System.out.println("the longest palindrome is "+longest);
+		
+		
+		String smallest=null;
+		for(String word:arr) {
+			if(isPalindrome(word)) {
+				if(smallest==null||word.length()<smallest.length())
+					smallest=word;
+			}
+		}
+		System.out.println("the smallest palindrome is "+smallest);
+
+		
+		
 	}
 	public static boolean isPalindrome(String s) {
 		StringBuffer buffer = new StringBuffer(s);
