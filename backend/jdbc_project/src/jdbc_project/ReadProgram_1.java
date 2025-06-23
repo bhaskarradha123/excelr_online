df509967916d8b507dc4ea1fb59e7f3959fb1dbc
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-23 18:11:54 +0530

    jdbc

---
+++ backend/jdbc_project/src/jdbc_project/ReadProgram_1.java
@@ -0,0 +1,5 @@
+package jdbc_project;
+
+public class ReadProgram_1 {
+
+}
---
+++ backend/jdbc_project/src/jdbc_project/Save_Program1.java
@@ -0,0 +1,8 @@
+package jdbc_project;
+
+public class Save_Program1 {
+
+	public static void main(String[] args) {
+		
+	}
+}
