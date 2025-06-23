1c5fd189cb6e6a7eda13342c2a956c54721fe5a0
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-23 18:52:37 +0530

    jdbc

--- backend/jdbc_project/src/jdbc_project/Save_Program1.java
+++ backend/jdbc_project/src/jdbc_project/Save_Program1.java
@@ -6,14 +6,20 @@ import java.sql.SQLException;
 import com.mysql.cj.jdbc.Driver;
 
 public class Save_Program1 {
-
 	public static void main(String[] args) {
 		try {
-			//Load and Register ---required packages		
+//			 Load and Register ---required packages
 			DriverManager.registerDriver(new Driver());
+//			 establish the connection
+			String url="jdbc:mysql://localhost:3306/your_database_Name";
+			String userName="root";
+			String pwd="root";
+			DriverManager.getConnection(url, userName, pwd);
+			
+			
 			System.out.println("done,,,");
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 		}
 	}
 }
