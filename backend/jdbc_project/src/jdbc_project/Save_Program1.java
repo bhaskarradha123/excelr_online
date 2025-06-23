552af7271716a4a2619b6c200f8954235aca58ea
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-23 18:28:52 +0530

    jdbc

--- backend/jdbc_project/src/jdbc_project/Save_Program1.java
+++ backend/jdbc_project/src/jdbc_project/Save_Program1.java
@@ -1,8 +1,16 @@
 package jdbc_project;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Save_Program1 {
 
 	public static void main(String[] args) {
-		
+		try {
+			//Load and Register ---required packages		
+			DriverManager.registerDriver(null);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
