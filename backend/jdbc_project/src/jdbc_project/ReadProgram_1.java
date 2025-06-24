4cc04187f4f503a884d46a74c6f2c688f9d4b454
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-24 18:07:43 +0530

    jdbc day2

--- backend/jdbc_project/src/jdbc_project/ReadProgram_1.java
+++ backend/jdbc_project/src/jdbc_project/ReadProgram_1.java
@@ -1,5 +1,26 @@
 package jdbc_project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ReadProgram_1 {
 
+	public static void main(String[] args) throws SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "root");
+		PreparedStatement ps = con.prepareStatement("select * from table_1");
+		ResultSet res = ps.executeQuery();
+		while (res.next()) {
+			int id = res.getInt("id");
+			String name = res.getString("name");
+			String email = res.getString(3);
+			long mobile = res.getLong(4);
+			String address = res.getString(5);
+
+			System.out.println(id+"--"+name+"--"+email+"--"+mobile+"--"+address);
+		}
+
+	}
 }
--- backend/jdbc_project/src/jdbc_project/UpdateProgram1.java
+++ backend/jdbc_project/src/jdbc_project/UpdateProgram1.java
@@ -15,7 +15,7 @@ public class UpdateProgram1 {
 		String address=sc.next();
 		System.out.println("enter id");
 		int id=sc.nextInt();
-		DriverManager.registerDriver(new Driver());
+//		DriverManager.registerDriver(new Driver());
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "root");
 		PreparedStatement ps = con.prepareStatement("update table_1 set email=? , address=? where id=?");
 		ps.setString(1, email);
@@ -25,7 +25,7 @@ public class UpdateProgram1 {
 			System.out.println("updated ...");
 		else
 			System.out.println("there is no matching record to update");
-		ps.close();
-		con.close();
+//		ps.close();
+//		con.close();
 	}
 }
