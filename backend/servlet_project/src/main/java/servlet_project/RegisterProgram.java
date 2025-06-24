12814af4fe8a612197b9a24d5c05f735006f54ac
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-24 20:06:08 +0530

    jdbc day2

--- backend/servlet_project/src/main/java/servlet_project/RegisterProgram.java
+++ backend/servlet_project/src/main/java/servlet_project/RegisterProgram.java
@@ -1,12 +1,17 @@
 package servlet_project;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+import servlet_project.dao.CrudOperation;
+
+@WebServlet("/save") //deployment descriptor - which helps to connect a req with a servlet class
 public class RegisterProgram  extends GenericServlet{
 
 	@Override
@@ -21,6 +26,17 @@ public class RegisterProgram  extends GenericServlet{
 	long phone_=Long.parseLong(phone);//parsing-converting String into long
 	
 	System.out.println(id_+ name+email+phone_+address);
+	
+	CrudOperation operation=new CrudOperation();
+	try {
+		if(operation.save(id_, name, email, phone_, address)>0)
+			System.out.println("data is inserted");
+		else
+			System.out.println("data not inserted");
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
 	}
 
---
+++ backend/servlet_project/src/main/java/servlet_project/dao/CrudOperation.java
@@ -0,0 +1,24 @@
+package servlet_project.dao; //data access object --which deals with db logic
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class CrudOperation {
+//  creating a  db logic  to save
+	public int save(int id, String name, String email, long phone, String address) throws SQLException {
+		DriverManager.registerDriver(new Driver());
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "root");
+		PreparedStatement ps = con.prepareStatement("insert into table_1 values(?,?,?,?,?)");
+		ps.setInt(1, id);
+		ps.setString(2, name);
+		ps.setString(2, email);
+		ps.setLong(4, phone);
+		ps.setString(5, address);
+		return ps.executeUpdate();
+	}
+
+}
