5b6b918109de2aee4d4995ea37bb7d470988a0ea
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-25 18:29:36 +0530

    jdbc3

--- backend/servlet_project/src/main/java/servlet_project/LoginProgram.java
+++ backend/servlet_project/src/main/java/servlet_project/LoginProgram.java
@@ -2,11 +2,14 @@ package servlet_project;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import servlet_project.dao.CrudOperation;
 @WebServlet("/login")
 public class LoginProgram extends HttpServlet {
 
@@ -16,6 +19,21 @@ public class LoginProgram extends HttpServlet {
 		long phone=Long.parseLong(req.getParameter("phone"));
 		
 		System.out.println(name+" "+phone);
+		CrudOperation operation=new CrudOperation();
+		try {
+		if(operation.login(name, phone))
+		{
+			RequestDispatcher dispatcher=req.getRequestDispatcher("success.jsp");
+			dispatcher.forward(req, resp);
+		}
+		else {
+			RequestDispatcher dispatcher=req.getRequestDispatcher("login.jsp");
+			dispatcher.include(req, resp);
+		}
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
