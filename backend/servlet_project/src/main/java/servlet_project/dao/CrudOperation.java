662ddf09f388b0d0c5e8a9443fd7a45cad2a4820
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-25 18:19:22 +0530

    jdbc3

--- backend/servlet_project/src/main/java/servlet_project/dao/CrudOperation.java
+++ backend/servlet_project/src/main/java/servlet_project/dao/CrudOperation.java
@@ -3,15 +3,21 @@ package servlet_project.dao; //data access object --which deals with db logic
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.mysql.cj.jdbc.Driver;
 
 public class CrudOperation {
 //  creating a  db logic  to save
-	public int save(int id, String name, String email, long phone, String address) throws SQLException {
+	
+	public Connection getConnection() throws SQLException {
 		DriverManager.registerDriver(new Driver());
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root", "root");
+		return con;
+	}	
+	public int save(int id, String name, String email, long phone, String address) throws SQLException {
+		Connection con=getConnection();
 		PreparedStatement ps = con.prepareStatement("insert into table_1 values(?,?,?,?,?)");
 		ps.setInt(1, id);
 		ps.setString(2, name);
@@ -19,6 +25,14 @@ public class CrudOperation {
 		ps.setLong(4, phone);
 		ps.setString(5, address);
 		return ps.executeUpdate();
+	}
+	public boolean login(String name , long phone) throws SQLException {
+		Connection con = getConnection();
+		PreparedStatement ps = con.prepareStatement("select * from table_1 where name=? and phone=?");
+		ps.setString(1, name);
+		ps.setLong(2, phone);
+		ResultSet rs = ps.executeQuery();
+		return rs.next();
 	}
 
 }
