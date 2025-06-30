a30e4c80640f37b362b12fa70a4ccd9488696afe
Excelr Solutions <mailmeasradha@gmail.com>
2025-06-30 19:06:57 +0530

    spring day1

--- backend/springs/spring_1/src/main/java/spring_1/Demo1.java
+++ backend/springs/spring_1/src/main/java/spring_1/Demo1.java
@@ -6,20 +6,24 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+
 public class Demo1 {
 
 	public static void main(String[] args) throws SQLException {
-	  Demo1 demo1=new Demo1();
-	  System.out.println(demo1); 
-	  Demo1 demo2=new Demo1();
-	  System.out.println(demo2);
-	  System.out.println("-----------------------");
-	  
-//	  creating a core container to create objects in it. 
-	  Resource r= new ClassPathResource("");
-	  BeanFactory bf=new XmlBeanFactory(r);
-	  System.out.println(bf);
+		Demo1 demo1 = new Demo1();
+		System.out.println(demo1);
+		Demo1 demo2 = new Demo1();
+		System.out.println(demo2);
+		System.out.println("-----------------------");
 
-	  
+//	  creating a core container to create objects in it. 
+		Resource r = new ClassPathResource("bean.xml");
+		BeanFactory bf = new XmlBeanFactory(r);
+		System.out.println(bf);
+		Demo1 d3 = bf.getBean(Demo1.class);
+		System.out.println(d3);
+		Demo1 d4 = bf.getBean(Demo1.class);
+		System.out.println(d4);
+
 	}
 }
