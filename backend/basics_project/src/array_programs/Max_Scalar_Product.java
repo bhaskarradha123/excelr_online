535aab354ae9e73f3ed98c6febc6a9d2c3e1ddde
Excelr Solutions <mailmeasradha@gmail.com>
2025-07-01 19:55:06 +0530

    spring day2

---
+++ backend/basics_project/src/array_programs/Max_Scalar_Product.java
@@ -0,0 +1,27 @@
+package array_programs;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Max_Scalar_Product {
+
+	public static void main(String[] args) {
+		int []arr1= {1,2,-4,5,2,6,8};
+		Integer []arr2= {4,5,2,9,7,5,4};
+		
+		if(arr1.length==arr2.length) {
+			Arrays.sort(arr1);
+			Arrays.sort(arr2);
+			int max=0;
+			for(int i=0;i<arr1.length;i++) {
+				max+=arr1[i]*arr2[i];
+			}
+			
+			System.out.println("minimum scalar product is "+max);
+			
+		}else {
+			System.out.println("can't find min sclar bcoz both the arrays are not in same size");
+		}
+	}
+}
