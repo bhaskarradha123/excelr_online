345c9866370413217142becc72884b235f30a55a
Excelr Solutions <mailmeasradha@gmail.com>
2025-07-02 18:02:58 +0530

    springboot

--- backend/springs/spring_2/src/main/java/com/excelR/spring_2/controller/EmployeeController.java
+++ backend/springs/spring_2/src/main/java/com/excelR/spring_2/controller/EmployeeController.java
@@ -2,11 +2,34 @@ package com.excelR.spring_2.controller;
 // used to handle  user req 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.excelR.spring_2.model.Employee;
 
 //@Component
 //@Controller //--req and res
 @RestController // for rest apis
 public class EmployeeController {
-
+//    http://localhost:8080/m1
+	@RequestMapping("/m1")
+	public void m1() {
+		System.out.println("m1 req is hitted ");
+	}
+//	  http://localhost:8080/m2
+	@RequestMapping("/m2")
+	public String m2() {
+		return "hello welcome to spring sessions";
+	}
+//  http://localhost:8080/m3?msg=hello hi by bye
+	@RequestMapping("/m3")
+	public void m3(String msg) {
+		System.out.println(msg);
+	}
+//	http://localhost:8080/m4
+	@RequestMapping("/m4")
+	public void m4(@RequestBody Employee employee) {
+		System.out.println(employee);
+	}
 }
--- backend/springs/spring_2/src/main/java/com/excelR/spring_2/model/Employee.java
+++ backend/springs/spring_2/src/main/java/com/excelR/spring_2/model/Employee.java
@@ -1,7 +1,64 @@
 package com.excelR.spring_2.model; 
+import org.springframework.beans.factory.annotation.Value;
 // blue prints and model class
 import org.springframework.stereotype.Component;
+
+
 @Component
 public class Employee {
+//	@Value("Radha")
+	private String name;
+	private String email;
+	private long phone;
+	private String pwd;
+	
+	
+	public String getName() {
+		return name;
+	}
 
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public long getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+
+	public String getPwd() {
+		return pwd;
+	}
+
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", email=" + email + ", phone=" + phone + ", pwd=" + pwd + "]";
+	}
+	
+	
+	
+	 
 }
