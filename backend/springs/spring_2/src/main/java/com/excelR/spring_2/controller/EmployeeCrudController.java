7eb8b7f3a67043284bad91607fbd0319af06c227
Excelr Solutions <mailmeasradha@gmail.com>
2025-07-02 19:11:01 +0530

    springboot

--- backend/springs/spring_2/src/main/java/com/excelR/spring_2/controller/EmployeeCrudController.java
+++ backend/springs/spring_2/src/main/java/com/excelR/spring_2/controller/EmployeeCrudController.java
@@ -2,9 +2,14 @@ package com.excelR.spring_2.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.excelR.spring_2.model.Employee;
@@ -12,28 +17,44 @@ import com.excelR.spring_2.model.Employee;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeCrudController {
-	
 //	http://localhost:8080/employee/save
 	@PostMapping("/save")
 	public Employee saveEmployee(@RequestBody Employee employee) {
 		System.out.println(employee);
 		return null;
 	}
-	public Employee loginEmployee() {
+//  http://localhost:8080/employee/login
+	@PostMapping("/login")
+	public Employee loginEmployee(@RequestBody Employee employee) {
+		System.out.println(employee.getEmail()+" "+ employee.getPwd());
 		return null;
 	}
-	public Employee updateEmployee() {
+//	 http://localhost:8080/employee/update
+	@PutMapping("/update")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		System.out.println(employee);
 		return null;
 	}
-	public Employee deleteEmployee() {
+//	 http://localhost:8080/employee/delete/101
+	@DeleteMapping("/delete/{id}")
+	public Employee deleteEmployee(@PathVariable int id) {
+		System.out.println(id);
 		return null;
 	}
-	public Employee fetchEmployeeById() {
+// http://localhost:8080/employee/fetchById?id=1001
+	@GetMapping("/fetchById")
+	public Employee fetchEmployeeById(@RequestParam int id) {
+		System.out.println(id);
 		 return null;
 	}
-	public Employee fetchEmployeeByEmail() {
+//	http://localhost:8080/employee/fetchByEmail/radha@gmail.com
+	@GetMapping("/fetchByEmail/{email}")
+	public Employee fetchEmployeeByEmail(@PathVariable String email) {
+		System.out.println(email);
 		 return null;
 	}
+//	http://localhost:8080/employee/fetchAll
+	@GetMapping("/fetchAll")
 	public List<Employee> fetchAllEmployee() {
 		 return null;
 	}
