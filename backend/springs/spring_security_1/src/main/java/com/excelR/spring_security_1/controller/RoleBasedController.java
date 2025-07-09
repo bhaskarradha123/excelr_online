1e749039591d0ffb5cb3e7f9b6d4b948be538c22
Excelr Solutions <mailmeasradha@gmail.com>
2025-07-09 17:57:10 +0530

    spring security

--- backend/springs/spring_security_1/src/main/java/com/excelR/spring_security_1/controller/RoleBasedController.java
+++ backend/springs/spring_security_1/src/main/java/com/excelR/spring_security_1/controller/RoleBasedController.java
@@ -1,10 +1,24 @@
 package com.excelR.spring_security_1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.excelR.spring_security_1.util.JwtUtil;
 
 @RestController
 public class RoleBasedController {
+	@Autowired
+	JwtUtil util;
+	
+	@PostMapping("/authenticate")
+	public String genertaeTokenWhenLogin(@RequestParam String userName) {
+	  return util.generateToken(userName);	
+	}
+	
+	
 //	http://localhost:8080/admin
     @GetMapping("/admin")
 	public String adminAccess() {
---
+++ backend/springs/spring_security_1/src/main/java/com/excelR/spring_security_1/util/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.excelR.spring_security_1.util;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+	private static final String SECRET_KEY = "mysecretkeymysecretkey";
+
+	public String generateToken(String username) {
+		return Jwts.builder().setSubject(username).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+				.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+	}
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+		return claimsResolver.apply(claims);
+	}
+
+	public boolean isTokenValid(String token, String username) {
+		return username.equals(extractUsername(token)) && !isTokenExpired(token);
+	}
+
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+}
