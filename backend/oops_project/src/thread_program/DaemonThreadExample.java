3192cd0ef27d76f6f2e03b2c2346e4da772ab073
Radha <122531010+bhaskarradha123@users.noreply.github.com>
2025-09-15 21:32:20 +0530

    Update DaemonThreadExample.java

--- backend/oops_project/src/thread_program/DaemonThreadExample.java
+++ backend/oops_project/src/thread_program/DaemonThreadExample.java
@@ -3,16 +3,30 @@ package thread_program;
 public class DaemonThreadExample {
 
 	public static void main(String[] args) {
-		Thread daemonThread=new Thread(()->{
-			while(true) {
-				System.out.println("daemonThread running ......");
-			}
-		});
-		
-	    daemonThread.setDaemon(true);
-		daemonThread.start();
-		
-		System.out.println("main thread ");
+		 Thread t=new  Thread(()->{
+            while(true){
+                try {
+                System.out.println("deamon thread is running");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+       
+        Thread user=new  Thread(()->{
+            System.out.println("user thread is running");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("user thread is completed");
+        });
+        user.start();
+        user.join();
 		
 	}
 }
